521405662bd7fa50cb67b2f0417f07f2016d2583
Barbie <barbie@missbarbell.co.uk>
2014-07-18 20:01:02 +0100

    whitespace cleanup

--- lib/Lingua/EN/NameCase.pm
+++ lib/Lingua/EN/NameCase.pm
@@ -1,21 +1,21 @@
-package Lingua::EN::NameCase ;
+package Lingua::EN::NameCase;
 
-use strict ;
-use locale ;
+use strict;
+use locale;
 
-use vars qw( $VERSION @ISA @EXPORT @EXPORT_OK $SPANISH ) ;
+use vars qw( $VERSION @ISA @EXPORT @EXPORT_OK $SPANISH );
 
-$VERSION = '1.15' ;
+$VERSION = '1.15';
 
 #--------------------------------------------------------------------------
 # Modules
 
-use Carp ;
-use Exporter() ;
+use Carp;
+use Exporter();
 
-@ISA        = qw( Exporter ) ;
-@EXPORT     = qw( nc ) ;
-@EXPORT_OK  = qw( NameCase nc ) ;
+@ISA        = qw( Exporter );
+@EXPORT     = qw( nc );
+@EXPORT_OK  = qw( NameCase nc );
 
 #--------------------------------------------------------------------------
 # Variables
@@ -26,109 +26,107 @@ $SPANISH    = 0;
 # Functions
 
 sub NameCase {
-
     croak "Usage: \$SCALAR|\@ARRAY = NameCase [\\]\$SCALAR|\@ARRAY"
-        if ref $_[0] and ( ref $_[0] ne 'ARRAY' and ref $_[0] ne 'SCALAR' ) ;
-        
-    local( $_ ) ;
+        if ref $_[0] and ( ref $_[0] ne 'ARRAY' and ref $_[0] ne 'SCALAR' );
+
+    local( $_ );
 
     if( wantarray and ( scalar @_ > 1 or ref $_[0] eq 'ARRAY' ) ) {
         # We have received an array or array reference in a list context
         # so we will return an array.
-        map { nc( $_ ) } @{ ref( $_[0] ) ? $_[0] : \@_ } ;
-    } 
-    elsif( ref $_[0] eq 'ARRAY' ) {
+        map { nc( $_ ) } @{ ref( $_[0] ) ? $_[0] : \@_ };
+
+    } elsif( ref $_[0] eq 'ARRAY' ) {
         # We have received an array reference in a scalar or void context
         # so we will work on the array in-place.
-        foreach ( @{ $_[0] } ) { 
-            $_ = nc( $_ ) ;
+        foreach ( @{ $_[0] } ) {
+            $_ = nc( $_ );
         }
-    }
-    elsif( ref $_[0] eq 'SCALAR' ) {
-        # We don't work on scalar references in-place; we take the value 
+
+    } elsif( ref $_[0] eq 'SCALAR' ) {
+        # We don't work on scalar references in-place; we take the value
         # and return a name-cased copy.
-        nc( ${ $_[0] } ) ;
-    }
-    elsif( scalar @_ == 1 and not ref $_[0] ) {
+        nc( ${ $_[0] } );
+
+    } elsif( scalar @_ == 1 and not ref $_[0] ) {
         # We've received a scalar: we return a name-cased copy.
-        nc( $_[0] ) ;
-    }
-    else {
-        croak "NameCase only accepts a single scalar, array or array ref" ;
+        nc( $_[0] );
+
+    } else {
+        croak "NameCase only accepts a single scalar, array or array ref";
     }
 }
 
 sub nc {
-
     croak "Usage: nc [[\\]\$SCALAR]"
-        if scalar @_ > 1 or ( ref $_[0] and ref $_[0] ne 'SCALAR' ) ;
-        
-    local( $_ ) = @_ if @_ ;
+        if scalar @_ > 1 or ( ref $_[0] and ref $_[0] ne 'SCALAR' );
+
+    local( $_ ) = @_ if @_;
     $_ = ${$_} if ref( $_ ) ;           # Replace reference with value.
 
     $_ = lc ;                           # Lowercase the lot.
-    s{ \b (\w)   }{\u$1}gox ;           # Uppercase first letter of every word.
-    s{ (\'\w) \b }{\L$1}gox ;           # Lowercase 's.
+    s{ \b (\w)   }{\u$1}gx;             # Uppercase first letter of every word.
+    s{ (\'\w) \b }{\L$1}gx;             # Lowercase 's.
 
     # Name case Mcs and Macs - taken straight from NameParse.pm incl. comments.
     # Exclude names with 1-2 letters after prefix like Mack, Macky, Mace
     # Exclude names ending in a,c,i,o, or j are typically Polish or Italian
 
-    if ( /\bMac[A-Za-z]{2,}[^aciozj]\b/o or /\bMc/o ) {
-        s/\b(Ma?c)([A-Za-z]+)/$1\u$2/go ;
+    if ( /\bMac[A-Za-z]{2,}[^aciozj]\b/ or /\bMc/ ) {
+        s/\b(Ma?c)([A-Za-z]+)/$1\u$2/g;
 
         # Now correct for "Mac" exceptions
-        s/\bMacEvicius/Macevicius/go ;  # Lithuanian
-        s/\bMacHado/Machado/go ;        # Portuguese
-        s/\bMacHar/Machar/go ;
-        s/\bMacHin/Machin/go ;
-        s/\bMacHlin/Machlin/go ;
-        s/\bMacIas/Macias/go ;  
-        s/\bMacIulis/Maciulis/go ;  
-        s/\bMacKie/Mackie/go ;
-        s/\bMacKle/Mackle/go ;
-        s/\bMacKlin/Macklin/go ;
-        s/\bMacQuarie/Macquarie/go ;
-        s/\bMacOmber/Macomber/go ;
-        s/\bMacIn/Macin/go ;
-        s/\bMacKintosh/Mackintosh/go ;
-        s/\bMacKen/Macken/go ;
-        s/\bMacHen/Machen/go ;
-        s/\bMacisaac/MacIsaac/go ;
-        s/\bMacHiel/Machiel/go ;
-        s/\bMacIol/Maciol/go ;
-        s/\bMacKell/Mackell/go ;
-        s/\bMacKlem/Macklem/go ;
-        s/\bMacKrell/Mackrell/go ;
-        s/\bMacLin/Maclin/go ;
-        s/\bMacKey/Mackey/go ;
-        s/\bMacKley/Mackley/go ;
-        s/\bMacHell/Machell/go ;
-        s/\bMacHon/Machon/go ;
+        s/\bMacEvicius/Macevicius/g;    # Lithuanian
+        s/\bMacHado/Machado/g;          # Portuguese
+        s/\bMacHar/Machar/g;
+        s/\bMacHin/Machin/g;
+        s/\bMacHlin/Machlin/g;
+        s/\bMacIas/Macias/g;
+        s/\bMacIulis/Maciulis/g;
+        s/\bMacKie/Mackie/g;
+        s/\bMacKle/Mackle/g;
+        s/\bMacKlin/Macklin/g;
+        s/\bMacQuarie/Macquarie/g;
+        s/\bMacOmber/Macomber/g;
+        s/\bMacIn/Macin/g;
+        s/\bMacKintosh/Mackintosh/g;
+        s/\bMacKen/Macken/g;
+        s/\bMacHen/Machen/g;
+        s/\bMacisaac/MacIsaac/g;
+        s/\bMacHiel/Machiel/g;
+        s/\bMacIol/Maciol/g;
+        s/\bMacKell/Mackell/g;
+        s/\bMacKlem/Macklem/g;
+        s/\bMacKrell/Mackrell/g;
+        s/\bMacLin/Maclin/g;
+        s/\bMacKey/Mackey/g;
+        s/\bMacKley/Mackley/g;
+        s/\bMacHell/Machell/g;
+        s/\bMacHon/Machon/g;
     }
-    s/Macmurdo/MacMurdo/go ;
- 
+    s/Macmurdo/MacMurdo/g;
+
     # Fixes for "son (daughter) of" etc. in various languages.
-    s{ \b Al(?=\s+\w)  }{al}gox ;                   # al Arabic or forename Al.
-    s{ \b Ap        \b }{ap}gox ;                   # ap Welsh.
-    s{ \b Ben(?=\s+\w) }{ben}gox ;                  # ben Hebrew or forename Ben.
-    s{ \b Dell([ae])\b }{dell$1}gox ;               # della and delle Italian.
-    s{ \b D([aeiu]) \b }{d$1}gox ;                  # da, de, di Italian; du French.
-    s{ \b De([lr])  \b }{de$1}gox ;                 # del Italian; der Dutch/Flemish.
-    s{ \b El        \b }{el}gox unless $SPANISH ;   # el Greek or El Spanish.
-    s{ \b La        \b }{la}gox unless $SPANISH ;   # la French or La Spanish.
-    s{ \b L([eo])   \b }{l$1}gox ;                  # lo Italian; le French.
-    s{ \b Van(?=\s+\w) }{van}gox ;                  # van German or forename Van.
-    s{ \b Von       \b }{von}gox ;                  # von Dutch/Flemish
+    s{ \b Al(?=\s+\w)  }{al}gx;                     # al Arabic or forename Al.
+    s{ \b Ap        \b }{ap}gx;                     # ap Welsh.
+    s{ \b Ben(?=\s+\w) }{ben}gx;                    # ben Hebrew or forename Ben.
+    s{ \b Dell([ae])\b }{dell$1}gx;                 # della and delle Italian.
+    s{ \b D([aeiu]) \b }{d$1}gx;                    # da, de, di Italian; du French.
+    s{ \b De([lr])  \b }{de$1}gx;                   # del Italian; der Dutch/Flemish.
+    s{ \b El        \b }{el}gx  unless $SPANISH;    # el Greek or El Spanish.
+    s{ \b La        \b }{la}gx  unless $SPANISH;    # la French or La Spanish.
+    s{ \b L([eo])   \b }{l$1}gx;                    # lo Italian; le French.
+    s{ \b Van(?=\s+\w) }{van}gx;                    # van German or forename Van.
+    s{ \b Von       \b }{von}gx;                    # von Dutch/Flemish
 
     # Fixes for roman numeral names, e.g. Henry VIII, up to 89, LXXXIX
     s{ \b ( (?: [Xx]{1,3} | [Xx][Ll]   | [Ll][Xx]{0,3} )?
-            (?: [Ii]{1,3} | [Ii][VvXx] | [Vv][Ii]{0,3} )? ) \b }{\U$1}gox ;
+            (?: [Ii]{1,3} | [Ii][VvXx] | [Vv][Ii]{0,3} )? ) \b }{\U$1}gx;
 
-    $_ ;
+    $_;
 }
 
-1 ;
+1;
 
 __END__
 
@@ -142,21 +140,21 @@ Lingua::EN::NameCase - Correctly case a person's name from UPERCASE or lowcase
 
     # Working with scalars; complementing lc and uc.
 
-    use Lingua::EN::NameCase qw( nc ) ;
+    use Lingua::EN::NameCase qw( nc );
 
-    $FixedCasedName  = nc( $OriginalName ) ;
+    $FixedCasedName  = nc( $OriginalName );
 
-    $FixedCasedName  = nc( \$OriginalName ) ;
+    $FixedCasedName  = nc( \$OriginalName );
 
     # Working with arrays or array references.
 
-    use Lingua::EN::NameCase 'NameCase' ;
+    use Lingua::EN::NameCase 'NameCase';
 
-    $FixedCasedName  = NameCase( $OriginalName ) ;
-    @FixedCasedNames = NameCase( @OriginalNames ) ;
+    $FixedCasedName  = NameCase( $OriginalName );
+    @FixedCasedNames = NameCase( @OriginalNames );
 
-    $FixedCasedName  = NameCase( \$OriginalName ) ;
-    @FixedCasedNames = NameCase( \@OriginalNames ) ;
+    $FixedCasedName  = NameCase( \$OriginalName );
+    @FixedCasedNames = NameCase( \@OriginalNames );
 
     NameCase( \@OriginalNames ) ; # In-place.
 
@@ -180,7 +178,7 @@ correctly with them.
 NameCase currently correctly name cases names which include any of the
 following:
 
-    Mc, Mac, al, el, ap, da, de, delle, della, di, du, del, der, 
+    Mc, Mac, al, el, ap, da, de, delle, della, di, du, del, der,
     la, le, lo, van and von.
 
 It correctly deals with names which contain apostrophies and hyphens too.
@@ -221,7 +219,7 @@ Use Kim Ryan's NameParse.pm for any really sophisticated name parsing.
 =head1 AUTHOR
 
   1998-2014    Mark Summerfield <summer@qtrac.eu>
-  2014-present Barbie <barbie@cpan.org>  
+  2014-present Barbie <barbie@cpan.org>
 
 =head1 ACKNOWLEDGEMENTS
 
